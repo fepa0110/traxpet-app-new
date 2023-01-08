486b56acd5e9c15876be7c46f12666919005d30d
Fabricio <fepafumi97@gmail.com>
2023-01-07 23:55:40 -0300

    Quitadas las comillas en nombres de constantes

--- constants/Colors.jsx
+++ constants/Colors.jsx
@@ -1,13 +1,13 @@
 export const ColorsApp = {
-    "primaryColor": "orangered",
-    "secondaryColor": "#FFF",
-    "terciaryColor": "indianred",
+    primaryColor: "orangered",
+    secondaryColor: "#FFF",
+    terciaryColor: "indianred",
 
-    "errorColor": "red",
-    "primaryBackgroundColor": "#FFF",
-    "primaryTextColor": "#FFF",
-    "secondaryTextColor": "orangered",
-    "headerBackButtonColor": "#FFF",
+    errorColor: "red",
+    primaryBackgroundColor: "#FFF",
+    primaryTextColor: "#FFF",
+    secondaryTextColor: "orangered",
+    headerBackButtonColor: "#FFF",
 
-    "naranjaClaro": "#FC832A",
+    naranjaClaro: "#FC832A",
 }
