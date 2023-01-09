801d7a2d7a8f8d73c1f4e8e444110f9e64672825
Fabricio <fepafumi97@gmail.com>
2023-01-09 20:51:24 -0300

    refactor: :fire: Cambiadas extensiones a jsx

--- constants/constants.js
+++
@@ -1,3 +0,0 @@
-// export const urlServer = "http://if012pf.fi.mdn.unp.edu.ar:28002/traxpet-server/rest"
-export const urlServer = "http://192.168.0.102:28002/traxpet-server/rest"
-export const urlServerBoosting = "http://192.168.0.102:28003"
