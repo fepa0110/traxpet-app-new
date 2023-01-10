f2b4811c96053f0c6353584f86ee9bbb17b1b455
Fabricio <fepafumi97@gmail.com>
2023-01-09 22:52:32 -0300

    feat: :recycle: [Header] Quitado componente derecho y documentado

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,26 @@
+/** Header 
+ * Props:
+ *  title: titulo del header
+ *  rightComponent: Componente del lado derecho
+ */
+
 import { React } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { ColorsApp } from "../constants/Colors";
-import { Header } from "@rneui/themed";
+
+import { Header as HeaderElements } from "@rneui/themed" ; 
+
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
-const TraxpetHeader = (props) => {
+
+const Header = (props) => {
   const navigation = useNavigation();
+
   const goBack = () => {
     navigation.goBack();
   };
 
-  const setRightComponent = () => {
+/*   const setRightComponent = () => {
     if (props.isRefresh) {
       return (
         <TouchableOpacity onPress={() => props.rightFunction()}>
@@ -23,10 +33,10 @@ const TraxpetHeader = (props) => {
       );
     }
     return null;
-  };
+  }; */
   return (
     <View>
-      <Header
+      <HeaderElements
         containerStyle={styles.headerContainer}
         leftComponent={
           <TouchableOpacity onPress={goBack}>
@@ -41,7 +51,7 @@ const TraxpetHeader = (props) => {
           text: props.title,
           style: styles.title,
         }}
-        rightComponent={setRightComponent()}
+        rightComponent={props.rightComponent}
       />
     </View>
   );
@@ -60,4 +70,4 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
-export default TraxpetHeader;
+export default Header;
