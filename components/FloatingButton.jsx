c0421d35d536ff08697df164af9ea078f135b49d
Fabricio <fepafumi97@gmail.com>
2023-01-14 23:45:07 -0300

    feat: :sparkles: Creado componente FloatingButton

---
+++ components/FloatingButton.jsx
@@ -0,0 +1,31 @@
+/**Boton flotante abajo a la derecha
+ * Props:
+ *  visible: mostrar boton
+ *  onPressFunction: onPress function
+ */
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import { FAB } from "@rneui/themed";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { ColorsApp } from "../constants/Colors";
+
+const FloatingButton = (props) => {
+    return (
+        <FAB
+            visible={props.visible}
+            placement="right"
+            icon={
+                <FontAwesome5
+                    name="plus"
+                    size={22}
+                    color={ColorsApp.secondaryColor}
+                />}
+            color={ColorsApp.primaryColor}
+            onPress={props.onPressFunction}
+        /> 
+    )
+}
+
+export default FloatingButton
