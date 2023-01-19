16dacb7dbfcf34c09c46a3e7a6a0ddc36af7f3c9
Fabricio <fepafumi97@gmail.com>
2023-01-19 18:08:48 -0300

    feat: :sparkles: [LocationService] Creado

---
+++ services/LocationService.jsx
@@ -0,0 +1,9 @@
+import { urlServer } from "../constants/constants";
+
+export async function getPublicationLocations(publicationId) {
+    return await fetch(
+        `${urlServer}/ubicaciones/publicacion/${publicationId}`
+    ).then((response) => {
+        return response.json();
+    });
+}
