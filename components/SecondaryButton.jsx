56a5640f7c106e94aea2ff518ec31608b6fbec04
Fabricio <fepafumi97@gmail.com>
2023-01-19 22:46:57 -0300

    feat: :sparkles: [SecondaryButton] Creado

---
+++ components/SecondaryButton.jsx
@@ -0,0 +1,43 @@
+/** Boton primario standard
+ * Props:
+ *  actionFunction: onPress function
+ *  title: Texto del boton
+ *  disabled: Deshabilitar el boton
+ */
+import { React } from "react";
+import { TouchableOpacity, StyleSheet, Text } from "react-native";
+import { ColorsApp } from "../constants/Colors";
+
+const PrimaryButton = (props) => {
+  return (
+    <TouchableOpacity
+      style={styles.secondaryButton}
+      onPress={props.actionFunction}
+      disabled={props.disabled}
+    >
+      <Text style={styles.textPrimaryButton}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  textPrimaryButton: {
+    color: ColorsApp.secondaryTextColor,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  secondaryButton: {
+    backgroundColor: ColorsApp.primaryBackgroundColor,
+    alignItems: "center",
+    alignContent: "center",
+
+    justifyContent: "center",
+    width: 115,
+    height: 40,
+    flexDirection: "row",
+    borderRadius: 50,
+    borderColor: ColorsApp.primaryColor,
+    borderWidth: 2,
+  },
+});
+export default PrimaryButton;
