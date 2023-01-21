f213426c6c191b040dc03428be3cc0ef72a015c5
Teo-Fer <ismafer0007@gmail.com>
2023-01-21 18:35:14 -0300

    feat: :zap: [PublicationService] Se genera service de publicaciones

--- services/PublicacionService.jsx
+++
@@ -1,8 +0,0 @@
-import { urlServer } from "../constants/constants";
-
-export async function getPublicacionesByUserRequest(username) {
-    return await fetch(`${urlServer}/publicaciones/usuario/${username}`)
-        .then((response) => {
-            return response.json()
-        })
-}
---
+++ services/PublicationService.jsx
@@ -0,0 +1,26 @@
+import { urlServer } from "../constants/constants";
+
+export async function getPublicacionesByUserRequest(username) {
+  return await fetch(`${urlServer}/publicaciones/usuario/${username}`).then(
+    (response) => {
+      return response.json();
+    }
+  );
+}
+
+export async function sendPublication(post) {
+  try {
+    const responsePublication = await fetch(urlServer + "/publicaciones", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post.publication),
+    });
+
+    return await responsePublication.json();
+  } catch (error) {
+    console.log("error ", error);
+  }
+}
