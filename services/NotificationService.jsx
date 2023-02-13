74f83c72e92104cab0ac0aeb0d0cbd2a12ef8318
Fabricio <fepafumi97@gmail.com>
2023-02-12 22:27:22 -0300

    refactor: :recycle: [NotificationService] Refactorizado metodo de notificacion

--- services/NotificationService.jsx
+++ services/NotificationService.jsx
@@ -1,25 +1,26 @@
 import { urlServer } from "../constants/constants";
 
 export async function readNotification(notificacion) {
-  try {
-    const response = await fetch(urlServer + "/notificaciones", {
-      method: "PUT", // or 'PUT'
-      body: JSON.stringify(notification),
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.log("Ha ocurrido un error");
-  }
+	return await fetch(urlServer + "/notificaciones", {
+		method: "PUT", // or 'PUT'
+		body: JSON.stringify(notificacion),
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((error) => {
+			console.log("Ha ocurrido un error");
+		});
 }
 
 export async function getNotificacionesByUserIdRequest(userId) {
-  return await fetch(`${urlServer}/notificaciones/usuario/${userId}`).then(
-    (response) => {
-      return response.json();
-    }
-  );
+	return await fetch(`${urlServer}/notificaciones/usuario/${userId}`).then(
+		(response) => {
+			return response.json();
+		}
+	);
 }
