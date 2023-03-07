0148790f6a3825939023321ff80b796df8daf4bd
Fabricio <fepafumi97@gmail.com>
2023-03-06 23:23:05 -0300

    feat: :lipstick: [SearchScreen] Agregado Header

--- screens/SearchScreen.jsx
+++ screens/SearchScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import HeaderHome from '../components/HeaderHome'
 
 const SearchScreen = () => {
     return (
         <View>
-        <Text>SearchScreen</Text>
+            <HeaderHome title="Buscar"/>
         </View>
     )
 }
