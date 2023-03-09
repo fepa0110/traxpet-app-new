7c14e80803e7bc0525083f833f79259c6ed05e04
Teo-Fer <ismafer0007@gmail.com>
2023-03-09 10:26:15 -0300

    fix:

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,10 @@
 import {
-	View,
-	Text,
-	StyleSheet,
-	TouchableOpacity,
-	ScrollView,
-	RefreshControl,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  RefreshControl,
 } from "react-native";
 import React, { useEffect, useState, useCallback } from "react";
 
@@ -26,200 +26,201 @@ import { getNotificacionesByUserIdRequest } from "../services/NotificationServic
 import FloatingButton from "../components/FloatingButton";
 
 const HomeScreen = () => {
-	const user = {
-		id: useSelector((state) => state.user.id),
-		username: useSelector((state) => state.user.username),
-	};
+  const user = {
+    id: useSelector((state) => state.user.id),
+    username: useSelector((state) => state.user.username),
+  };
 
-	const [refreshing, setRefreshing] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-	const navigation = useNavigation();
-	const [publicaciones, setPublicaciones] = useState([]);
-	const [notificaciones, setNotificaciones] = useState([]);
-	const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [publicaciones, setPublicaciones] = useState([]);
+  const [notificaciones, setNotificaciones] = useState([]);
+  const dispatch = useDispatch();
 
-	const getPublicaciones = async () => {
-		let publicacionesUser = await getPublicacionesByUserRequest(
-			user.username
-		);
+  const getPublicaciones = async () => {
+    let publicacionesUser = await getPublicacionesByUserRequest(user.username);
 
-		setPublicaciones(publicacionesUser.data);
-	};
+    setPublicaciones(publicacionesUser.data);
+  };
 
-	const getNotificaciones = async () => {
-		let notificacionesUser = await getNotificacionesByUserIdRequest(user.id);
+  const getNotificaciones = async () => {
+    let notificacionesUser = await getNotificacionesByUserIdRequest(user.id);
 
-		setNotificaciones(notificacionesUser.data);
-	};
+    setNotificaciones(notificacionesUser.data);
+  };
 
-	useEffect(() => {
-		getPublicaciones();
-		getNotificaciones();
-	}, []);
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getPublicaciones();
+      getNotificaciones();
+    });
+    return unsubscribe;
+  }, [navigation]);
 
-	const onRefresh = useCallback(() => {
-		setRefreshing(true);
-		getPublicaciones();
-		getNotificaciones();
-		setTimeout(() => {
-			setRefreshing(false);
-		}, 2000);
-	}, []);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    getPublicaciones();
+    getNotificaciones();
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
 
-	const listEmpty = () => {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.message}>
-					El usuario aun no ha realizado publicaciones
-				</Text>
-				<FontAwesome5 name="paw" size={32} color={ColorsApp.primaryColor} />
-			</View>
-		);
-	};
+  const listEmpty = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          El usuario aun no ha realizado publicaciones
+        </Text>
+        <FontAwesome5 name="paw" size={32} color={ColorsApp.primaryColor} />
+      </View>
+    );
+  };
 
-	const showPublications = () => {
-		return (
-			<FlashList
-				contentContainerStyle={{ paddingVertical: 20 }}
-				data={publicaciones}
-				renderItem={renderItem}
-				estimatedItemSize={60}
-				ListEmptyComponent={listEmpty}
-			/>
-		);
-	};
+  const showPublications = () => {
+    return (
+      <FlashList
+        contentContainerStyle={{ paddingVertical: 20 }}
+        data={publicaciones}
+        renderItem={renderItem}
+        estimatedItemSize={60}
+        ListEmptyComponent={listEmpty}
+      />
+    );
+  };
 
-	const renderItem = ({ item }) => (
-		<View>
-			<TouchableOpacity
-				style={{
-					flexDirection: "row",
-					marginVertical: 5,
-				}}
-				onPress={() => {
-					navigation.navigate("UserPublicationNavigation", {
-						screen: "UserPublicationScreen",
-						params: {
-							publication: item,
-						},
-					});
-				}}>
-				<View style={{ marginHorizontal: 20 }}>
-					<FontAwesome5
-						name={
-							item.tipoPublicacion === "MASCOTA_ENCONTRADA"
-								? "map-marked-alt"
-								: "search-location"
-						}
-						size={item.tipoPublicacion === "MASCOTA_ENCONTRADA" ? 28 : 32}
-						color={ColorsApp.primaryColor}
-					/>
-				</View>
-				<View>
-					<Text style={styles.itemTitle}>
-						{item.mascota.especie.nombre}
-					</Text>
-					<Text style={styles.itemSubtitle}>
-						{(item.tipoPublicacion === "MASCOTA_ENCONTRADA"
-							? "Mascota encontrada"
-							: "Mascota buscada") +
-							"\nCreada: " +
-							item.fechaPublicacion +
-							"\nModificada: " +
-							item.fechaModificacion}
-					</Text>
-				</View>
-			</TouchableOpacity>
-		</View>
-	);
+  const renderItem = ({ item }) => (
+    <View>
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          marginVertical: 5,
+        }}
+        onPress={() => {
+          navigation.navigate("UserPublicationNavigation", {
+            screen: "UserPublicationScreen",
+            params: {
+              publication: item,
+            },
+          });
+        }}
+      >
+        <View style={{ marginHorizontal: 20 }}>
+          <FontAwesome5
+            name={
+              item.tipoPublicacion === "MASCOTA_ENCONTRADA"
+                ? "map-marked-alt"
+                : "search-location"
+            }
+            size={item.tipoPublicacion === "MASCOTA_ENCONTRADA" ? 28 : 32}
+            color={ColorsApp.primaryColor}
+          />
+        </View>
+        <View>
+          <Text style={styles.itemTitle}>{item.mascota.especie.nombre}</Text>
+          <Text style={styles.itemSubtitle}>
+            {(item.tipoPublicacion === "MASCOTA_ENCONTRADA"
+              ? "Mascota encontrada"
+              : "Mascota buscada") +
+              "\nCreada: " +
+              item.fechaPublicacion +
+              "\nModificada: " +
+              item.fechaModificacion}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 
-	return (
-		<View
-			style={{
-				height: "100%",
-				backgroundColor: ColorsApp.primaryBackgroundColor,
-			}}>
-			<HeaderHome
-				rightComponent={
-					<TouchableOpacity
-						onPress={() => {
-							navigation.navigate("NotificationsScreen");
-						}}
-						style={styles.headerRightComponent}>
-						<FontAwesome
-							name={notificaciones.length == 0 ? "bell-o" : "bell"}
-							size={25}
-							color={ColorsApp.primaryColor}
-						/>
-						<Text style={styles.textNotification}>
-							{notificaciones.length}
-						</Text>
-					</TouchableOpacity>
-				}
-			/>
-			<ScrollView
-				refreshControl={
-					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-				}>
-				{showPublications()}
-			</ScrollView>
-			<FloatingButton
-				visible={true}
-				onPressFunction={() => {
-					dispatch(resetNewPublication());
-					navigation.navigate("PublicationNavigation", {
-						screen: "PublicationBasicDataScreen",
-					});
-				}}
-			/>
-		</View>
-	);
+  return (
+    <View
+      style={{
+        height: "100%",
+        backgroundColor: ColorsApp.primaryBackgroundColor,
+      }}
+    >
+      <HeaderHome
+        rightComponent={
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("NotificationsScreen");
+            }}
+            style={styles.headerRightComponent}
+          >
+            <FontAwesome
+              name={notificaciones.length == 0 ? "bell-o" : "bell"}
+              size={25}
+              color={ColorsApp.primaryColor}
+            />
+            <Text style={styles.textNotification}>{notificaciones.length}</Text>
+          </TouchableOpacity>
+        }
+      />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {showPublications()}
+      </ScrollView>
+      <FloatingButton
+        visible={true}
+        onPressFunction={() => {
+          dispatch(resetNewPublication());
+          navigation.navigate("PublicationNavigation", {
+            screen: "PublicationBasicDataScreen",
+          });
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	headerRightComponent: {
-		paddingTop: 10,
-		flexDirection: "row",
-	},
-	textNotification: {
-		flex: 2,
-		fontSize: 18,
-		color: ColorsApp.primaryColor,
-		fontWeight: "bold",
-	},
-	floatingButtonContainer: {
-		justifyContent: "flex-end",
-		position: "absolute",
-		bottom: 20,
-		right: 20,
-	},
-	button: {
-		backgroundColor: ColorsApp.primaryColor,
-		borderRadius: 55,
-		alignContent: "center",
-		alignItems: "center",
-		width: 55,
-		height: 55,
-	},
-	container: { justifyContent: "center", alignItems: "center" },
-	item: {
-		backgroundColor: "#f9c2ff",
-	},
-	itemTitle: {
-		fontSize: 15,
-		fontWeight: "bold",
-		color: ColorsApp.primaryTextColor,
-	},
-	itemSubtitle: {
-		fontSize: 13,
-		color: ColorsApp.secondaryTextColor,
-	},
-	message: {
-		fontWeight: "bold",
-		fontSize: 16,
-		padding: 5,
-		color: ColorsApp.primaryTextColor,
-	},
+  headerRightComponent: {
+    paddingTop: 10,
+    flexDirection: "row",
+  },
+  textNotification: {
+    flex: 2,
+    fontSize: 18,
+    color: ColorsApp.primaryColor,
+    fontWeight: "bold",
+  },
+  floatingButtonContainer: {
+    justifyContent: "flex-end",
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  button: {
+    backgroundColor: ColorsApp.primaryColor,
+    borderRadius: 55,
+    alignContent: "center",
+    alignItems: "center",
+    width: 55,
+    height: 55,
+  },
+  container: { justifyContent: "center", alignItems: "center" },
+  item: {
+    backgroundColor: "#f9c2ff",
+  },
+  itemTitle: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: ColorsApp.primaryTextColor,
+  },
+  itemSubtitle: {
+    fontSize: 13,
+    color: ColorsApp.secondaryTextColor,
+  },
+  message: {
+    fontWeight: "bold",
+    fontSize: 16,
+    padding: 5,
+    color: ColorsApp.primaryTextColor,
+  },
 });
 
 export default HomeScreen;
